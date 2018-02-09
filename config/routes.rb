eea7ffd427eeeed510107612f14f01c93da610ae
fengfanhua <fengfanhua@meituan.com>
2018-02-09 19:59:13 +0800

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 	
+  get    'login', to: 'sessions#new'
+  post   'login', to: 'sessions#create'
+  delete 'logout', to: 'seesions#destroy'
+
   root 'home#index'
 
   get 'articles', to: 'front_article#index'
